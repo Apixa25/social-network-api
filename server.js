45482d6b1d23fb233bde538214cb8523ad962c80
stevensillsii <stevensills2@gmail.com>
2023-11-23 19:32:05 -0800

    server.js setup with everything needed to start an express server and a mongo database

---
+++ server.js
@@ -0,0 +1,35 @@
+// Require express and mongoose
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Create an Express application
+const app = express();
+
+// Define the port to listen on, using process.env.PORT or defaulting to 3000
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse incoming requests with urlencoded or JSON payloads
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
+// Connect to MongoDB using Mongoose, with the default database named "workout"
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// Log Mongoose queries to the console for debugging purposes
+mongoose.set("debug", true);
+
+// Import and use the routes defined in the "./routes" directory
+app.use(require("./routes"));
+
+// Start the Express server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
